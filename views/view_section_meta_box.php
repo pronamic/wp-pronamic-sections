d988c5abbe41a2b7a839877cc312eb31c1cdd498
Leon Rowland <leon@rowland.nl>
2013-05-24 12:00:27 +0200

    update domain

--- views/view_section_meta_box.php
+++ views/view_section_meta_box.php
@@ -1,5 +1,5 @@
 <?php echo $nonce; ?>
-<a href="#" class="button button-primary js-pronamic-sections-add-tab"><?php _e( 'Add tab', 'pronamic-ct-domain' ); ?></a>
+<a href="#" class="button button-primary js-pronamic-sections-add-tab"><?php _e( 'Add tab', 'pronamic-sections-domain' ); ?></a>
 <input type="hidden" name="pronamic_section_quantity" value="<?php echo $quantity; ?>" class="js-pronamic-sections-quantity"/>
 <div class="pct-tabs-holder js-pronamic-sections-holder" style="padding-top:10px;padding-bottom:10px;">
 
@@ -9,7 +9,7 @@
 		<div class="pct-tab" style="margin-bottom:10px;border:1px solid #ccc;padding:10px;">
 			<input type="hidden" name="pct_content[<?php echo $i; ?>][order]" value="<?php echo esc_attr( $i ); ?>" class="js-pronamic-sections-order"/>
 			<div class="post-tab-title">
-				<h3><?php _e( 'Tab Title', 'pronamic-ct-domain' ); ?></h3> 
+				<h3><?php _e( 'Tab Title', 'pronamic-sections-domain' ); ?></h3> 
 				<input type="text" name="pct_content[<?php echo esc_attr( $i ); ?>][title]" class="widefat" value="<?php echo esc_attr( $title ); ?>"/>
 			</div>
 			<div class="post-tab-content js-pronamic-sections-collapse">
