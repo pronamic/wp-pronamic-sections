f0ae71347214d69f56fb6db10c34371c0d8bb060
Leon Rowland <leon@rowland.nl>
2013-05-24 12:01:38 +0200

    update domain

--- views/view_section_meta_box.php
+++ views/view_section_meta_box.php
@@ -10,7 +10,7 @@
 		<div class="pct-tab" style="margin-bottom:10px;border:1px solid #ccc;padding:10px;">
 			<input type="hidden" name="pct_content[<?php echo $i; ?>][order]" value="<?php echo esc_attr( $i ); ?>" class="js-pronamic-sections-order"/>
 			<div class="post-tab-title">
-				<h3><?php _e( 'Section Title', 'pronamic-ct-domain' ); ?></h3> 
+				<h3><?php _e( 'Section Title', 'pronamic-sections-domain' ); ?></h3> 
 				<input type="text" name="pct_content[<?php echo esc_attr( $i ); ?>][title]" class="widefat" value="<?php echo esc_attr( $title ); ?>"/>
 			</div>
 			<div class="post-tab-content js-pronamic-sections-collapse">
@@ -23,4 +23,4 @@
 	<?php endfor; ?>
 </div>
 
-<a href="#" class="button button-primary js-pronamic-sections-add-tab"><?php _e( 'Add new section', 'pronamic-ct-domain' ); ?></a>
+<a href="#" class="button button-primary js-pronamic-sections-add-tab"><?php _e( 'Add new section', 'pronamic-sections-domain' ); ?></a>
