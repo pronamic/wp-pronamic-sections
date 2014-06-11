7807c75ceafbc80e1d46b9a2d610fe5414dd3923
Remco Tolsma <info@remcotolsma.nl>
2014-06-11 09:11:49 +0200

    Added Grunt with some tasks.

---
+++ Gruntfile.js
@@ -0,0 +1,83 @@
+module.exports = function( grunt ) {
+	// Project configuration.
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+		// PHPLint
+		phplint: {
+			options: {
+				phpArgs: {
+					'-lf': null
+				}
+			},
+			all: [ 'classes/**/*.php' ]
+		},
+
+		// PHP Code Sniffer
+		phpcs: {
+			application: {
+				dir: [ './' ],
+			},
+			options: {
+				standard: 'project.ruleset.xml',
+				extensions: 'php',
+				ignore: 'wp-svn,deploy,node_modules'
+			}
+		},
+
+		// JSHint
+		jshint: {
+			files: ['Gruntfile.js', 'admin/js/*.js' ],
+			options: {
+				// options here to override JSHint defaults
+				globals: {
+					jQuery: true,
+					console: true,
+					module: true,
+					document: true
+				}
+			}
+		},
+
+		// Check WordPress version
+		checkwpversion: {
+			options: {
+				readme: 'readme.txt',
+				plugin: 'pronamic-ideal.php',
+			},
+			check: {
+				version1: 'plugin',
+				version2: 'readme',
+				compare: '=='
+			},
+			check2: {
+				version1: 'plugin',
+				version2: '<%= pkg.version %>',
+				compare: '=='
+			}
+		},
+
+		// Make POT
+		makepot: {
+			target: {
+				options: {
+					cwd: '',
+					domainPath: 'languages',
+					type: 'wp-plugin',
+					exclude: [ 'deploy/.*', 'wp-svn/.*' ],
+				}
+			}
+		},
+	} );
+
+	grunt.loadNpmTasks( 'grunt-phplint' );
+	grunt.loadNpmTasks( 'grunt-phpcs' );
+	grunt.loadNpmTasks( 'grunt-phpunit' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-checkwpversion' );
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+
+	// Default task(s).
+	grunt.registerTask( 'default', [ 'jshint', 'phplint', 'phpcs', 'checkwpversion' ] );
+	grunt.registerTask( 'pot', [ 'makepot' ] );
+};
